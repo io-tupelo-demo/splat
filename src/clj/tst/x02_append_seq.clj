0d2987061220974104904e142f68aa38b5121251
Alan Thompson <cloojure@gmail.com>
2023-01-25 00:00:25 -0800

    misc

--- src/clj/tst/x02_append_seq.clj
+++ src/clj/tst/x02_append_seq.clj
@@ -8,9 +8,20 @@
 (defn safe-even? [arg] (and (int? arg) (even? arg)))
 (defn safe-odd? [arg] (and (int? arg) (odd? arg)))
 
+; For something so simple, you don't need Spectre or Splatter
 (verify
   (let [data       {:a [1 2 3]}
-        data-splat (splat/splatter data)
         result (update-in data [:a]  #(glue % [4 5]))
         ]
     (is= result {:a [1 2 3 4 5]})))
+
+; For something more complicated, Splatter helps.  Suppose we want to append
+; a value 666 to the end of every nested list?
+; #todo #awt finish this
+#_(verify
+  (let [data       {:a [1 2 3]
+                    :b {:c {:d [11 12 13]}}}
+        data-splat (splat/splatter data)
+        ]
+    (spyx-pretty data-splat )
+    ))
--- src/clj/tst/x09_nested_nil_remove.clj
+++ src/clj/tst/x09_nested_nil_remove.clj
@@ -9,12 +9,12 @@
 ; Remove all `nil` values from a nested sequences
 (verify
   (let [data       {:a [1 2 nil 3 nil [4 nil 5]]}
-        expected   {:a [1 2 3 [ 4 5]]}
+        expected   {:a [1 2 3 [4 5]]}
         data-splat (splat/splatter data)
         ]
     (is= data-splat
       {:entries
-       #{{:key {:data :a, :type :prim},
+       #{{:key  {:data :a, :type :prim},
           :type :map/entry,
           :val
           {:entries
@@ -23,14 +23,12 @@
              {:idx 2, :type :list/entry, :val {:data nil, :type :prim}}
              {:idx 3, :type :list/entry, :val {:data 3, :type :prim}}
              {:idx 4, :type :list/entry, :val {:data nil, :type :prim}}
-             {:idx 5,
+             {:idx  5,
               :type :list/entry,
-              :val
-              {:entries
-               #{{:idx 0, :type :list/entry, :val {:data 4, :type :prim}}
-                 {:idx 1, :type :list/entry, :val {:data nil, :type :prim}}
-                 {:idx 2, :type :list/entry, :val {:data 5, :type :prim}}},
-               :type :coll/list}}},
+              :val  {:entries #{{:idx 0, :type :list/entry, :val {:data 4, :type :prim}}
+                                {:idx 1, :type :list/entry, :val {:data nil, :type :prim}}
+                                {:idx 2, :type :list/entry, :val {:data 5, :type :prim}}},
+                     :type    :coll/list}}},
            :type :coll/list}}},
        :type :coll/map}
       )
@@ -40,10 +38,10 @@
                            (spyq :-----------------------------------------------------------------------------)
                            (let-spy-pretty
                              [stack-pat [{:type :list/entry}]
-                                 result    (if (and (submatch? stack-pat stack)
-                                                 (nil? (:data node)))
-                                             nil
-                                             node)]
+                              result    (if (and (submatch? stack-pat stack)
+                                              (nil? (:data node)))
+                                          nil
+                                          node)]
                              result))}
           result (splat/splatter-walk intc data)]
       (is= (spyx-pretty result) expected))
