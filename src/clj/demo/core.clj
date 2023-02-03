802534cf9d2a6228b5c352951c2d821615b50b0a
Alan Thompson <cloojure@gmail.com>
2023-02-02 19:25:21 -0800

    misc

--- src/clj/demo/core.clj
+++ src/clj/demo/core.clj
@@ -7,12 +7,6 @@
 (defn safe-even? [arg] (and (int? arg) (even? arg)))
 (defn safe-odd? [arg] (and (int? arg) (odd? arg)))
 
-(s/defn add2 :- s/Num
-  "An example to demonstrate Plumatic Schema."
-  [x :- s/Num
-   y :- s/Num]
-  (+ x y))
-
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
